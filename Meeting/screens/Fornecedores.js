a42b82344c4e2d6a42d7b2d5360cd98107226097
Ruan Finamor <ruanfinamor@gmail.com>
2023-11-30 14:41:48 -0300

    Ajustando cores

--- Meeting/screens/Fornecedores.js
+++ Meeting/screens/Fornecedores.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-import { View, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, FlatList, Text } from 'react-native';
 
 import { MaterialIcons } from "@expo/vector-icons"
 import usuarios from '../data/usuarios';
@@ -10,44 +10,54 @@ export default props => {
 
     function Fornecedores({ item: usuario }) {
       return (
-        <ListItem 
-        style={styles.list}
-        key={usuario.id} 
-        bottomDivider
-        onPress={() => props.navigation.navigate('Cadastro')}
-        >          
-          <Avatar 
-            rounded 
-            source={{uri: usuario.avatar_url}} />
-          <ListItem.Content>
-            <ListItem.Title>{usuario.name}</ListItem.Title>
-            <ListItem.Subtitle>{usuario.adress}</ListItem.Subtitle>
-            <ListItem.Subtitle>{usuario.contact}</ListItem.Subtitle>
-            <ListItem.Subtitle>{usuario.category}</ListItem.Subtitle>
-          </ListItem.Content>
-        </ListItem>
-        
-      )
-    }  
-      return (
-        <View style={styles.container}>
-           <FlatList            
-            data={usuarios}
-            keyExtractor={usuario => usuario.id.toString()}
-            showsVerticalScrollIndicator={false}
-            renderItem={ Fornecedores }
-          />
-        </View>
+        <View style={styles.container2}>
+          <View>               
+            <ListItem    
+            linearGradientProps={{
+              colors: ["#FF9800", "#F44336"],
+              start: { x: 1, y: 0 },
+              end: { x: 0.2, y: 0 },
+            }}
+            key={usuario.id} 
+            bottomDivider
+            onPress={() => props.navigation.navigate('Cadastro')}            
+            >                  
+              <Avatar 
+                rounded 
+                source={{uri: usuario.avatar_url}}                              
+              />                            
+              <ListItem.Content style={styles.list}> 
+                <ListItem.Title style={styles.listItems}>{usuario.name}</ListItem.Title>
+                <ListItem.Subtitle style={styles.listItems}>{usuario.adress}</ListItem.Subtitle>
+                <ListItem.Subtitle style={styles.listItems}>{usuario.contact}</ListItem.Subtitle>
+                <ListItem.Subtitle style={styles.listItems}>{usuario.category}</ListItem.Subtitle>
+              </ListItem.Content>              
+            </ListItem>
+          </View> 
+        </View>    
+        )
+      }  
+        return (
+          <View style={styles.container}>
+            <FlatList                    
+              data={usuarios}
+              keyExtractor={usuario => usuario.id.toString()}
+              showsVerticalScrollIndicator={false}
+              renderItem={ Fornecedores }
+            />
+          </View>        
       )
     }
     
     const styles = StyleSheet.create({
       container: {
         flex: 1,
-        backgroundColor: '#191414',
-        alignItems: 'center',
-        justifyContent: 'center',  
-        paddingHorizontal: 18,  
+        backgroundColor: '#191414',        
+      },
+      container2: {       
+        width: '100%',
+        backgroundColor: '#191414',        
+        paddingVertical: 10,         
       },
       cadastro: {
         color: '#1DB954',
@@ -75,8 +85,11 @@ export default props => {
         fontSize: 18,
       },
       list: {
-       
-      }
+        backgroundColor: '#191414',           
+      },
+      listItems: {
+        color: '#1DB954',
+      },
     });
     
   /*=> ( 
